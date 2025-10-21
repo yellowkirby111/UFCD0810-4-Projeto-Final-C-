a62def8ec01afe4e1b2d783b8831ba837175c83f
Duarte Ferreira Diog <a35494@aepombal.edu.pt>
2025-10-21 10:44:13 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,7 @@ int main() {
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Clothing Store App");
+    InitWindow(screenWidth, screenHeight, "Pepka");
     SetTargetFPS(60);
 
     // Window mode handling: support Windowed, Windowed-Fullscreen (bordered window resized to monitor),
